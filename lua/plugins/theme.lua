b226aa3e2483f3908a216ac4b57c654833acf313
Mohit Kumar Agrawal <mohit.agrawal@rubrik.com>
2025-12-04 09:39:20 +0530

    Something

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+			commentStyle = { italic = false },
+			keywordStyle = { italic = false },
+			overrides = function(colors)
+				local theme = colors.theme
+				local makeDiagnosticColor = function(color)
+					local c = require("kanagawa.lib.color")
+					return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+				end
+				return {
+					NormalFloat = { bg = "none" },
+					FloatTitle = { bg = "none" },
+					FloatBorder = { bg = "NONE", fg = colors.palette.springBlue },
+					DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+					DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+					DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+					DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency,,
+					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+					PmenuSbar = { bg = theme.ui.bg_m1 },
+					PmenuThumb = { bg = "#C0A36E" },
+					BlinkCmpMenuBorder = { fg = "NONE", bg = "NONE" },
+					CursorLineNr = { fg = colors.palette.sakuraPink, bg = "NONE" },
+					CursorLine = { fg = "NONE", bg = colors.palette.sumiInk4 },
+					IblIndent = { fg = colors.palette.sumiInk4, bg = "NONE" },
+					IblWhitespace = { fg = colors.palette.sumiInk4, bg = "NONE" },
+					IblScope = { fg = colors.palette.sumiInk5, bg = "NONE" },
+				}
+			end,
+		},
+		lazy = false,
+		priority = 1000,
+		config = function(_, opts)
+			require("kanagawa").setup(opts)
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+}
